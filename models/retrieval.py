2a3353720630c1a3e47b14ebd6028993815a7723
SANJANA RAVISHAKAR <1rn21cs139.sanjanar@rnsit.ac.in>
2024-07-21 12:26:41 +0530

    Update retrieval.py

--- models/retrieval.py
+++ models/retrieval.py
@@ -1,1 +1,23 @@
+retrive
 
+def convert_seq(df,feature):
+#     text = ' '.join(list(vocab.keys()))
+    t = Tokenizer()
+    entries = [entry for entry in df[feature]]
+    print(entries)
+    print('----')
+    t.fit_on_texts(entries)
+    joblib.dump(t, path_to_dumps+'tokenizer_t.pkl')   # why a pkl file
+    vocab_size = len(t.word_index) +1 # +1 for oov 
+    print(t.word_index)
+    entries = [entry for entry in df[feature]]
+    max_length = max([len(s.split()) for s in entries])
+    print('----')
+    print("max length of string is : ",max_length)
+    print('----')
+    encoded = t.texts_to_sequences(entries)
+    print(encoded)
+    padded = pad_sequences(encoded, maxlen=max_length, padding='post')
+    print('----')
+    print(padded)
+    return padded, vocab_size
