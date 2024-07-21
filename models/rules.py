c3f8ac894278ded296b923b0716ca0c3a034ba44
SANJANA RAVISHAKAR <1rn21cs139.sanjanar@rnsit.ac.in>
2024-07-21 12:27:02 +0530

    Update rules.py

--- models/rules.py
+++ models/rules.py
@@ -1,1 +1,24 @@
+rule
 
+def text_normalization(text):
+    text = str(text).lower()                        # text to lower case
+    spl_char_text = re.sub(r'[^ a-z]','',text)      # removing special characters
+    tokens = nltk.word_tokenize(spl_char_text)      # word tokenizing
+    lema = wordnet.WordNetLemmatizer()              # intializing lemmatization
+    tags_list = pos_tag(tokens,tagset=None)         # parts of speech
+    lema_words = []                                 # empty list 
+    for token,pos_token in tags_list:               # lemmatize according to POS
+        if pos_token.startswith('V'):               # Verb
+            pos_val = 'v'
+        elif pos_token.startswith('J'):             # Adjective
+            pos_val = 'a'
+        elif pos_token.startswith('R'):             # Adverb
+            pos_val = 'r'
+        else:
+            pos_val = 'n'                           # Noun
+        lema_token = lema.lemmatize(token,pos_val)
+
+        if lema_token in stop: 
+          lema_words.append(lema_token)             # appending the lemmatized token into a list
+    
+    return " ".join(lema_words)
